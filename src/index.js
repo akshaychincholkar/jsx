c3fd383d7f4e85badaaa1d4f6ab001600c0477f3
Akshay Chincholkar <akshaychincholkar@gmail.com>
2023-03-14 11:45:45 +0530

    basic react structures and props

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 // these are libraries used to show components on the screen
 import React from "react"; // knows how multiple components work together
 import ReactDOM from "react-dom/client"; // knows how to get component to show up in the browser
-
+import MyApp from "./App";
 // 2. Get a reference to div with ID root
     // index.html contains  <div id="root"></div>
 const el = document.getElementById('root');
@@ -12,13 +12,8 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 // 4. Create a component
-function App(){
-    let message = 'Bye There';
-    if(Math.random() > 0.5){
-        message = 'Hi There';
-    }
-    return <h1>{message}</h1>;
-}
+    //Seperate file is created as App.js
+
 // 5. Show the component on the screen
-root.render(<App/>);
+root.render(<MyApp/>);
 
