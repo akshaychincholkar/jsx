d9a6a3067fac783785ffb7e4c1b9ada8676cf5ab
akshaychincholkar <akshaychincholkar@gmail.com>
2024-10-20 16:12:45 +0530

    initial jsx

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@
 // these are libraries used to show components on the screen
 import React from "react"; // knows how multiple components work together
 import ReactDOM from "react-dom/client"; // knows how to get component to show up in the browser
-import MyApp from "./App";
+// import MyApp from "./App1";
+import MyApp from "./1_jsx_intro"; // Thus, name of the file is imported 
+
+
+// import App from "./App";
 // 2. Get a reference to div with ID root
     // index.html contains  <div id="root"></div>
 const el = document.getElementById('root');
@@ -15,5 +19,6 @@ const root = ReactDOM.createRoot(el);
     //Seperate file is created as App.js
 
 // 5. Show the component on the screen
-root.render(<MyApp/>);
 
+root.render(<MyApp/>);
+// root.render(<App/>);
