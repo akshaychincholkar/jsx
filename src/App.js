5c58645bc729118ece76eb66f27797f57fe4bda0
akshaychincholkar <akshaychincholkar@gmail.com>
2024-10-20 15:47:39 +0530

    refactored

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { ReactDOM } from "react";
-function App(){
-    let message = 'Bye There';
-    if(Math.random() > 0.5){
-        message = 'Hi There';
-    }
-    return (
-        <div>
-            <h1>{message}</h1>
-            <h2>{ new Date().toLocaleTimeString() }</h2>
-            <input type = "number" min={5}/>
-            <input type = "text" min={5} spellCheck={true}/>
-            <textarea autoFocus={true}/>
-        </div>
-    );
-}
-export default App;
+// import React from "react";
+// import { ReactDOM } from "react";
+// function App(){
+//     let message = 'Bye There';
+//     if(Math.random() > 0.5){
+//         message = 'Hi There';
+//     }
+//     return (
+//         <div>
+//             <h1>{message}</h1>
+//             <h2>{ new Date().toLocaleTimeString() }</h2>
+//             {/* <input type = "number" min={5}/>
+//             <input type = "text" min={5} spellCheck={true}/> */}
+//             <textarea autoFocus={true}/>
+//         </div>
+//     );
+// }
+// export default App;
