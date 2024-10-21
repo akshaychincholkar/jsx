356a151041f734d080f5c2eef7b5b24cad5ef86c
akshaychincholkar <akshaychincholkar@gmail.com>
2024-10-21 19:36:35 +0530

    jsx intro

---
+++ src/1_jsx_intro.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { ReactDOM } from "react";
+import ParentComponents from "./2_1_props";
+
+function App1 (){
+
+ const year = new Date().getFullYear();;
+
+ const element = <h1>This is {year}</h1>;
+
+ 
+
+ return (
+
+    //  {element}
+    <div><h1>Ganpati Bappa Mourya!</h1>
+    <h1>{year}</h1>
+    <h1>Props in React.js</h1>
+    <ParentComponents/>
+    </div>
+    
+
+ );
+
+};
+
+ 
+
+export default App1;
